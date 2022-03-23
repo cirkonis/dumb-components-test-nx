711a5e6cc85048cdfcff8da9baeebb2270d69c04
mima <mima@computas.com>
2022-03-23 09:37:10 +0100

    abstracted away the bottons in the card component

--- libs/shared-design-components-test-1/src/lib/shared-design-components-test.module.ts
+++ libs/shared-design-components-test-1/src/lib/shared-design-components-test.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { CardComponent } from './card/card.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    ButtonComponent
   ],
   exports: [
     HeaderComponent,
-    CardComponent
+    CardComponent,
+    ButtonComponent
   ],
 })
 export class SharedDesignComponentsTestModule {}
