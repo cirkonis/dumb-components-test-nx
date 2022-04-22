d1a14c726c73752176d3b9ec962f71c1d600d2e8
mima <mima@computas.com>
2022-04-22 13:09:11 +0200

    styling added to table and one off width values added to table and card for storybook rendering

--- libs/shared-design-components-test-1/src/lib/card/card.component.ts
+++ libs/shared-design-components-test-1/src/lib/card/card.component.ts
@@ -3,7 +3,7 @@ import {Component, Input} from '@angular/core';
 @Component({
   selector: 'dumb-components-test-card',
   template: `
-    <div class="h-full flex flex-col p-lg bg-secondary-light shadow-md hover:shadow-lg">
+    <div class="h-full flex flex-col p-lg bg-secondary-light shadow-md hover:shadow-lg w-[20rem]">
       <div class="pb-md text-lg font-bold">{{ title }}</div>
       <p class="mb-xl flex-1">
         <ng-content select='[text]'></ng-content>
--- libs/shared-design-components-test-1/src/lib/tables/generic-table/generic-table.component.ts
+++ libs/shared-design-components-test-1/src/lib/tables/generic-table/generic-table.component.ts
@@ -4,23 +4,33 @@ import { ITDSColumnObject } from '../../../../../interfaces/src/lib/table-data-s
 @Component({
   selector: 'dumb-components-test-generic-table',
   template: `
-    <table
-      class='bg-white flex-1 w-auto'
-      cdk-table [dataSource]='dataSource'>
-      <!-- Header and Row Declarations -->
-      <tr
-        class='bg-secondary-light'
-        cdk-header-row *cdkHeaderRowDef='displayedColumns'></tr>
-      <tr cdk-row *cdkRowDef='let row; columns: displayedColumns'></tr>
-
-      <ng-container *ngFor='let column of columnObjects;let i = index' cdkColumnDef='{{column.columnDef}}'>
-        <th cdk-header-cell *cdkHeaderCellDef>{{column.header}}</th>
-        <td cdk-cell *cdkCellDef='let row'>{{column.cell(row)}}</td>
-      </ng-container>
-    </table>
-  `,
+<!--    FLEX CONTAINERS AND STYLING    -->
+    <div class='mt-md flex flex-col w-[62]'>
+      <div class='-my-md -mx-md sm:-mx-sm lg:-mx-lg'>
+        <div class='inline-block min-w-full py-sm align-middle'>
+          <div class='shadow-sm ring-1 ring-black ring-opacity-5'>
+<!--            TABLE                  -->
+            <table
+              class="min-w-full border-separate" style="border-spacing: 0"
+              cdk-table [dataSource]='dataSource'>
+              <!-- Header and Row Declarations -->
+              <tr
+                class='bg-secondary-light'
+                cdk-header-row *cdkHeaderRowDef='displayedColumns'></tr>
+              <tr cdk-row *cdkRowDef='let row; columns: displayedColumns'></tr>
+<!--              COLUMN HEADER AND CELL DEFINITIONS    -->
+              <ng-container *ngFor='let column of columnObjects;let i = index' cdkColumnDef='{{column.columnDef}}'>
+                <th scope="col" class='sticky top-0 z-10 border-b border-primary-dark bg-secondary-light bg-opacity-75 py-sm pl-sm pr-sm text-left text-sm font-semibold text-black backdrop-blur backdrop-filter sm:pl-sm lg:pl-lg' cdk-header-cell *cdkHeaderCellDef>{{column.header}}</th>
+                <td class="whitespace-nowrap border-b border-primary-light py-sm pl-sm pr-sm text-sm font-medium text-black sm:pl-sm lg:pl-lg" cdk-cell *cdkCellDef='let row'>{{column.cell(row)}}</td>
+              </ng-container>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  `
 })
-export class GenericTableComponent implements OnInit{
+export class GenericTableComponent implements OnInit {
   @Input() dataSource!: any[];
   @Input() columnObjects!: ITDSColumnObject[];
   @Input() displayedColumns: string[] = [];
